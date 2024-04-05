089416f8b7a50c4528baad3bba2e6070e85548a7
kevinsadrak <93287177+kevinsadrak@users.noreply.github.com>
2024-04-05 15:24:30 -0400

    Add files via upload

---
+++ monitor-2-chat/webhooksecrets.py
@@ -0,0 +1,16 @@
+# database password to the AGM database
+AGMDBPASSWORD = ""
+AGM_DB = "agm"
+AGM_DB_USER = "agm"
+AGM_DB_HOST = "localhost"
+AGM_DB_PORT = 5432
+AGM_DB_SCHEMA = "agm" #if you are using a different schema for the table change this, i.e public
+
+# secret you setup in monitor
+WEBHOOKSECRET = ""
+
+# WEB Hook URL to your favorite chat software channel
+TEAMS_WEBHOOK_URL = ""
+DISCORD_WEBHOOK_URL =""
+SLACK_WEBHOOK_URL = ""
+
