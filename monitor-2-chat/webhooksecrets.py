68114a06d825f71cdb46f8d32af4ad322de56c89
kevinsadrak <93287177+kevinsadrak@users.noreply.github.com>
2024-04-08 14:53:24 -0400

    Update webhooksecrets.py
    Removed three different webhook urls and  added the monitor domain

--- monitor-2-chat/webhooksecrets.py
+++ monitor-2-chat/webhooksecrets.py
@@ -6,11 +6,12 @@ AGM_DB_HOST = "localhost"
 AGM_DB_PORT = 5432
 AGM_DB_SCHEMA = "agm" #if you are using a different schema for the table change this, i.e public
 
+#this is the domain of the monitor server, it is used to build the link to the alert in monitor
+MONITOR_DOMAIN_NAME = ""  #don't forget the port if you need it!
+
 # secret you setup in monitor
 WEBHOOKSECRET = ""
 
 # WEB Hook URL to your favorite chat software channel
-TEAMS_WEBHOOK_URL = ""
-DISCORD_WEBHOOK_URL =""
-SLACK_WEBHOOK_URL = ""
+WEBHOOK_URL = ""
 
