d73d49f9058a6bf08a3edfbc95379516b7faa034
Ed Conrad <ed.conrad@dymaptic.com>
2024-12-18 16:12:16 -0700

    fix complex string quoting to successfully call Python script from PowerShell

--- webgisdr-reporting/webgisdr_full_backup.ps1
+++ webgisdr-reporting/webgisdr_full_backup.ps1
@@ -12,17 +12,16 @@ $jsonResults = Join-Path -Path $webgisdrDirectory -ChildPath "webgisdrResults.js
 Start-Process -FilePath $webgisdr -ArgumentList "--export --file `"$propertiesFile`" --output `"$jsonResults`"" -NoNewWindow
 
 # Define source and destination directories
-$sourceDirectory = "C:\webgisdr_backup"  # local location WebGISDR is saving backups
-$destinationDirectory = "C:\test"        # ultimately store backups elsewhere
+$sourceDirectory = "C:\webgisdr_backup"  # TODO local location WebGISDR is saving backups
+$destinationDirectory = "C:\test"        # TODO ultimately store backups elsewhere
 
-# After webgisdr is completed, move the backup to the intended location. 
+# After WebGISDR is completed, move the backup to the intended location.
 # we use the * wildcard since the name of the backup will be different each time this runs.
 $backups = Get-ChildItem $sourceDirectory -Filter *.webgissite
 $robocopyLogFile = Join-Path -Path $webgisdrDirectory -ChildPath "robocopyResults.log"
 
 foreach($file in $backups){
-	# /z option  
-	# Copies files in restartable mode. In restartable mode, should a file copy be interrupted, robocopy can pick up where it left off rather than recopying the entire file.
+	# /z option copies files in restartable mode. In restartable mode, should a file copy be interrupted, robocopy can pick up where it left off rather than recopying the entire file.
     robocopy $file.DirectoryName $destinationDirectory $file.name /z /log:"$robocopyLogFile"
 
     # If robocopy is successful, delete the backup in sourceDirectory. Note the following exit codes:
@@ -32,21 +31,20 @@ foreach($file in $backups){
 	# 2: There are some additional files in the destination directory that aren't present in the source directory. No files were copied.
     if ($LASTEXITCODE -le 2) {
         Remove-Item -Path $file.FullName -Force
-    }    
+    }
 }
-         
+
 # Delete backups older than 30 days.
-# Get-ChildItem -Path $destinationDirectory -Filter '*.webgissite' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-30) } | Remove-Item -Force
+Get-ChildItem -Path $destinationDirectory -Filter '*.webgissite' | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-30) } | Remove-Item -Force
 
 # Parse JSON results and send Slack notification
+# Note that both ArcGIS Server and ArcGIS Pro python environments have the keyring and requests installed by default; however, Portal does not.
+# Therefore, depending on whether your Portal is on its own machine or not, you may need to clone the existing Portal Python environment to install the missing modules
+# Default Python Locations
+# Portal:        C:\Program Files\ArcGIS\Portal\framework\runtime\python  # <-- Missing both keyring and requests!
+# ArcGIS Server: C:\Program Files\ArcGIS\Server\framework\runtime\ArcGIS\bin\Python\envs\arcgispro-py3
+# ArcGIS Pro:    C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3
 $pythonLog = Join-Path -Path $webgisdrDirectory "webgisdr_notify.log"
-Write-Host "Start"
-# Start-Process -FilePath "C:\Program Files\ArcGIS\Portal\framework\runtime\python\pythonw.exe" -ArgumentList ".\webgisdr_notify.py `"C:\Program Files\ArcGIS\Portal\tools\webgisdr\webgisdrResults.json`"" -NoNewWindow # > $pythonLog 2>&1
-
-
-$pythonScript = Join-Path $webgisdrDirectory -ChildPath "webgisdr_notify.py"
-$params = $pythonScript, '--json_file', $jsonResults
-# & "C:\Program Files\ArcGIS\Portal\framework\runtime\python\pythonw.exe" $pythonScript --json_file $jsonResults -NoNewWindow > $pythonLog 2>&1
-& "C:\Program Files\ArcGIS\Portal\framework\runtime\python\pythonw.exe" @params -NoNewWindow > $pythonLog 2>&1
-Write-Host $LASTEXITCODE
-Write-Host "Finished"
+Start-Process -FilePath "C:\Program Files\ArcGIS\Server\framework\runtime\ArcGIS\bin\Python\envs\arcgispro-py3\pythonw.exe" `
+              -ArgumentList ".\webgisdr_notify.py", "--json_file", "`"$jsonResults`"" `
+              -NoNewWindow -Wait -RedirectStandardError "$pythonLog"
