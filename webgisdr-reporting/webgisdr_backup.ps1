3174d14fbd2ad527d23c5f6bfe298514f03bbef9
Ed Conrad <ed.conrad@dymaptic.com>
2024-12-31 10:42:59 -0700

    .properties file made into argument; added exception handling; added documentation

--- webgisdr-reporting/webgisdr_backup.ps1
+++ webgisdr-reporting/webgisdr_backup.ps1
@@ -1,19 +1,46 @@
-﻿# We're saving the Portal backup locally before copying it the location it needs to be stored, b/c the last step of bundling it up was failing. ESRI docs state the following:
-# During an export, if it is taking a long time to package the backup, consider setting the BACKUP_LOCATION to a local path.
-# You can then copy the finished package to its intended location. Make sure there is enough space on the local drive to store the backup temporarily.
+﻿<#
+Name: webgisdr_backup.ps1
+Purpose:
+    PowerShell script that is meant to be called twice from Windows Task Scheduler - once with a weekly trigger for
+    Full backups and the other with weeknight triggers for Incremental backups (note: a full backup must be run prior
+    to running an incremental backup).
+
+    The script accomplishes the following:
+    1.	Calls webgisdr.bat passing along either webgisdr_full.properties or webgisdr_incremental.properties.
+    2.	Use Microsoft’s robocopy to copy the backup saved locally to a different server.
+    3.	Remove any backups older than 30 days (or change to your organization's policy)
+    4.	Call a Python script to parse WebGISDR’s output JSON file and send notification to Slack channel.
+
+Usage:
+    Setup "Portal Full Backup" Windows Scheduled Task
+    powershell.exe -ExecutionPolicy Bypass -File webgisdr_backup.ps1 -PropertiesFile "path\to\webgisdr_full.properties"
+
+    Setup "Portal Incremental Backup" Windows Scheduled Task
+    powershell.exe -ExecutionPolicy Bypass -File webgisdr_backup.ps1 -PropertiesFile "path\to\webgisdr_incremental.properties"
+
+Assumptions:
+    This PowerShell script and the Python script, webgisdr_notify.py, reside in the same directory as webgisdr.bat
+    webgisdr.bat default location: C:\Program Files\ArcGIS\Portal\tools\webgisdr
+
+#>
+
+# Command line parameter with default value if none is provided
+param(
+    [string]$PropertiesFile = "C:\Program Files\ArcGIS\Portal\tools\webgisdr\webgisdr_full.properties"
+)
 
 # Define variables for WebGISDR
 $webgisdrDirectory = "C:\Program Files\ArcGIS\Portal\tools\webgisdr"
 $webgisdr = Join-Path -Path $webgisdrDirectory -ChildPath "webgisdr.bat"
-$propertiesFile = Join-Path -Path $webgisdrDirectory -ChildPath "webgisdr.properties"
 $jsonResults = Join-Path -Path $webgisdrDirectory -ChildPath "webgisdrResults.json"
 
 # Call ESRI's WebGIS Disaster and Recovery Utility with the export option, pointed to the properties file, and setup to create a JSON file.
-Start-Process -FilePath $webgisdr -ArgumentList "--export --file `"$propertiesFile`" --output `"$jsonResults`"" -NoNewWindow
+# run it synchronously by using -Wait (Start-Process runs asynchronously by default)
+Start-Process -FilePath $webgisdr -ArgumentList "--export --file `"$PropertiesFile`" --output `"$jsonResults`"" -NoNewWindow -Wait
 
-# Define source and destination directories
+# Define source and destination directories (Source )
 $sourceDirectory = "C:\webgisdr_backup"  # TODO local location WebGISDR is saving backups
-$destinationDirectory = "C:\test"        # TODO ultimately store backups elsewhere
+$destinationDirectory = "C:\test"        # TODO ultimately store backups on another server
 
 # After WebGISDR is completed, move the backup to the intended location.
 # we use the * wildcard since the name of the backup will be different each time this runs.
@@ -39,12 +66,11 @@ Get-ChildItem -Path $destinationDirectory -Filter '*.webgissite' | Where-Object 
 
 # Parse JSON results and send Slack notification
 # Note that both ArcGIS Server and ArcGIS Pro python environments have the keyring and requests installed by default; however, Portal does not.
-# Therefore, depending on whether your Portal is on its own machine or not, you may need to clone the existing Portal Python environment to install the missing modules
+# If Portal is on its own machine, you will need to install Python (Portal doesn't have conda nor does it have pip.exe where you could simply run 'pip install requests keyring')
 # Default Python Locations
 # Portal:        C:\Program Files\ArcGIS\Portal\framework\runtime\python  # <-- Missing both keyring and requests!
 # ArcGIS Server: C:\Program Files\ArcGIS\Server\framework\runtime\ArcGIS\bin\Python\envs\arcgispro-py3
 # ArcGIS Pro:    C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3
-$pythonLog = Join-Path -Path $webgisdrDirectory "webgisdr_notify.log"
 Start-Process -FilePath "C:\Program Files\ArcGIS\Server\framework\runtime\ArcGIS\bin\Python\envs\arcgispro-py3\pythonw.exe" `
               -ArgumentList ".\webgisdr_notify.py", "--json_file", "`"$jsonResults`"" `
-              -NoNewWindow -Wait -RedirectStandardError "$pythonLog"
+              -NoNewWindow -Wait
--- webgisdr-reporting/webgisdr_notify.py
+++ webgisdr-reporting/webgisdr_notify.py
@@ -12,10 +12,10 @@ Requirements:
     - Click the "Create New App" button and select "From scratch".
     - Provide an app name such as "Webgisdr Notification", select your Workspace, and select "Create App".
     - Under Features, select "Incoming Webhooks", activate them.
-    - Select "Add New Webhook to Workspace", choose the Slack Channel you want Notifications to appear, & select "Allow".
+    - Select "Add New Webhook to Workspace", choose the Slack Channel you want Notifications to appear & select "Allow".
     - Copy the Webhook URL that is generated and keep it secret.
     - First time this script runs, hardcode the URL and assign it to the webhook_url Python variable.
-    - Subsequently, change webhook_url back to an empty string so it's no longer in plain-text.
+    - Subsequently, change webhook_url back to an empty string, so it's no longer in plain-text.
 
 Author: Ed Conrad
 Created: 12/18/2024
@@ -24,78 +24,97 @@ Created: 12/18/2024
 import argparse
 import json
 import logging
-import sys
 import os
+import sys
+import traceback
 
 import keyring
 import requests
 
 
 def main():
-    print('hello')
-    log = os.path.join(os.path.dirname(__file__), "Webgisdr_Notify.log")
-    logging.basicConfig(filename=log, level=logging.DEBUG, filemode='w',
+    log = os.path.join(os.path.dirname(__file__), "Webgisdr_Notify_Python.log")
+    logging.basicConfig(filename=log, level=logging.INFO, filemode='w',
                         format='%(asctime)s %(levelname)s %(message)s',
                         datefmt='%#m/%#d/%Y %#I:%M:%S %p')
+    try:
+        # command-line code
+        parser = argparse.ArgumentParser(description='Python script parses the WebGISDR output JSON file and sends a '
+                                                     'Notification to Slack.')
+        parser.add_argument('--json_file', type=str,
+                            help='The WebGISDR output JSON file.')
+        args = parser.parse_args()
+        json_file = args.json_file
+        with open(json_file, 'r') as f:
+            results = json.load(f)
 
-    # command-line code
-    parser = argparse.ArgumentParser(description='Python script parses the WebGISDR output JSON file and sends a '
-                                                 'Notification to Slack.')
-    parser.add_argument('--json_file', type=str,
-                        help='The WebGISDR output JSON file.')
-    args = parser.parse_args()
-    json_file = args.json_file
-    with open(json_file, 'r') as f:
-        results = json.load(f)
+        # TODO Instructions to user:
+        # - First run only, provide the webhook_url in plain text.
+        # - Subsequently, change it back to an empty string to keep it secret.
+        webhook_url = ''
+        service_name = 'Slack_Hook_WebGISDR_Notification'
+        username = 'default'  # NOTE: the keyring module requires a username when saving a credential.
+        if webhook_url == '':
+            # Retrieve URL from Windows Credential Store
+            webhook_url = keyring.get_password(service_name=service_name, username=username)
+        else:
+            # Save URL to Windows Credential Store
+            keyring.set_password(service_name=service_name, username=username, password=webhook_url)
 
-    # TODO Instructions to user: First run only, provide the webhook_url in plain text. Subsequently, change it back to an empty string to keep it secret.
-    webhook_url = ''
-    service_name = 'Slack_Hook_WebGISDR_Notification'
-    username = 'default'  # NOTE: keyring requires a username when saving a credential (we don't use this anywhere else)
-    if webhook_url == '':
-        # Retrieve URL from Windows Credential Store
-        webhook_url = keyring.get_password(service_name=service_name, username=username)
-    else:
-        # Save URL to Windows Credential Store
-        keyring.set_password(service_name=service_name, username=username, password=webhook_url)
+        if webhook_url == '':
+            logging.error('Unable to post notification - missing Slack Webhook URL.')
+            sys.exit(1)
 
-    if webhook_url == '':
-        logging.error('Unable to post notification - missing Slack Webhook URL.')
-        sys.exit(1)
-
-    # Data structure follows Slack's formatting https://api.slack.com/reference/surfaces/formatting
-    payload = {
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": f"WebGISDR {results['operation'].upper()} {results['status'].upper()}\n"
-                            f"Elapsed Time: {results['elapsedTime']}\nZip Time: {results['zipTime']}"
-                    # "emoji": True
-                }
-            }
-        ]
-    }
-
-    # Get detailed results of the various backup components: Portal, Data Store, each federated instance of ArcGIS Server
-    for r in results['results']:
-        new_section = {
-            "type": "section",
-            "fields": [
+        # Data structure follows Slack's formatting https://api.slack.com/reference/surfaces/formatting
+        payload = {
+            "blocks": [
                 {
-                    "type": "mrkdwn",
-                    "text": f"*{r['name']}:*\n{r['URL']}\n{r['status'].upper()}\n{r['elapsedTime']}\n"
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"WebGISDR Mode: {results['operation'].upper()}"
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "*Overall Summary:*\n"
+                                    f"Result: {results['status'].upper()}\n"
+                                    f"Elapsed Time{results['elapsedTime']}\n"
+                                    f"Zip Time: {results['zipTime']}\n"
+                        }
+                    ]
                 }
             ]
         }
-        payload['blocks'].append(new_section)
 
-    response = requests.post(url=webhook_url, json=payload, headers={"Content-Type": "application/json"})
-    if response.status_code == 200:
-        logging.info('Successfully posted WebGISDR notification in Slack.')
-    else:
-        logging.error(f'Failed to post WebGISDR notification in Slack.\n{response.json()}')
+        # Get results of the various backup components: Portal, Data Store, each federated instance of ArcGIS Server
+        for r in results['results']:
+            new_section = {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*{r['name']}:*\n"
+                                f"{r['URL']}\n"
+                                f"Result: {r['status'].upper()}\n"
+                                f"Elapsed Time: {r['elapsedTime']}\n"
+                    }
+                ]
+            }
+            payload['blocks'].append(new_section)
+
+        response = requests.post(url=webhook_url, json=payload, headers={"Content-Type": "application/json"})
+        response.raise_for_status()
+        if response.status_code == 200:
+            logging.info('Successfully posted WebGISDR notification in Slack.')
+        else:
+            logging.error(f'Failed to post WebGISDR notification in Slack.\n{response.json()}')
+
+    except:
+        logging.error(f'\n{traceback.format_exc()}')
 
 
 if __name__ == '__main__':
