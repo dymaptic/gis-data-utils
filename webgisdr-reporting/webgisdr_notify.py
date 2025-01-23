a78a27e1e5799ea3c0be51dd9fa8f7a68304d0a2
Ed Conrad <ed.conrad@dymaptic.com>
2025-01-23 09:01:27 -0700

    error handling for missing JSON file; cleanup for clarity

--- webgisdr-reporting/webgisdr_notify.py
+++ webgisdr-reporting/webgisdr_notify.py
@@ -55,14 +55,22 @@ def main():
         # command-line code
         parser = argparse.ArgumentParser(description='Python script parses the WebGISDR output JSON file and sends a '
                                                      'Notification to Slack.')
-        parser.add_argument('--json_file', type=str,
+        parser.add_argument(name_or_flags='--json_file', type=str,
                             help='The WebGISDR output JSON file.')
-        parser.add_argument('--chat_software', type=str,
+        parser.add_argument(name_or_flags='--chat_software', type=str,
                             help='The location where the notification will go. Valid values are teams and slack.')
         args = parser.parse_args()
+
+        # Read the Portal backup JSON results file (i.e., the value of webgisdr.bat's --output parameter)
         json_file = args.json_file
-        with open(json_file, 'r') as f:
-            results = json.load(f)
+        if json_file is None:
+            raise ValueError('Missing value for the required --json_file parameter.')
+        try:
+            with open(json_file, 'r') as f:
+                results = json.load(f)
+        except FileNotFoundError:
+            logging.error(f'FileNotFoundError: unable to find {json_file}')
+            sys.exit(1)
 
         chat = args.chat_software
         if chat is None:
@@ -93,6 +101,7 @@ def main():
             logging.error('Unable to post notification - missing Webhook URL.')
             sys.exit(1)
 
+        # Place the WebGISDR results into the data structure expected by the chat software.
         payload = {}
         if chat == 'teams':
             payload = {
@@ -189,7 +198,7 @@ def main():
         response = requests.post(url=webhook_url, json=payload, headers={"Content-Type": "application/json"})
         response.raise_for_status()
 
-        # Slack returning 200 while Teams has been returning 202
+        # Slack is returning 200 while Teams has been returning both 200 and 202.
         if 200 <= response.status_code < 300:
             logging.info(f'Successfully posted WebGISDR notification in {chat}.')
         else:
